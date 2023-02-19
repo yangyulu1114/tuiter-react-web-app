bbfe128ec8692129ff6906a29e2e6deb1be2d90f
yangyulu <yangyulu1114@163.com>
2023-02-18 22:13:47 -0800

    homeScreen

---
+++ public/tuiter/HomeScreen/index.js
@@ -0,0 +1,19 @@
+/* eslint-env jquery */
+
+import NavigationSidebar from "../NavigationSidebar/index.js";
+import postSummaryList from "../PostSummaryList/index.js";
+
+$('#wd-home').append(`
+   <div class="row mt-2 mb-2">
+   <div class="col-2 col-md-2 col-lg-1 col-xl-2">
+<!--    <h3>NavigationSidebar</h3>-->
+        ${NavigationSidebar()}
+   </div>
+   <div class="col-10 col-lg-7 col-xl-6">
+<!--        central part-->
+   </div>
+   <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 ">
+        ${postSummaryList()}
+   </div>
+  </div>
+   `);

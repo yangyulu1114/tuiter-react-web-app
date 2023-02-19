702c39dbf11c017f82544c9a4f7bd2300c656813
yangyulu <yangyulu1114@163.com>
2023-02-19 10:24:28 -0800

    home Screen

--- public/tuiter/HomeScreen/index.js
+++ public/tuiter/HomeScreen/index.js
@@ -31,8 +31,70 @@ $('#wd-home').append(`
                                 <p class="mb-2">Watch Countdown: Inspiration4 Mission to Space | Netflix Official Site</p>
                                 <span class="text-dark">From training to launch to landing, this all-access docuseries rides along with
                                  the Inspiration4 crew on the first all-civilian orbital space mission.</span></br>
-                                 <span class="text-dark">netflix.com</span>
+                                 <i class="fa fa-link text-muted"> netflix.com</i>
                             </div>
+                        </div>
+                        
+                        <div>
+                            <ul class="list-group list-group-horizontal">
+                                <li class="list-group-item me-5">
+                                    <i class="fa fa-comment text-muted">  4.2K</i>
+                                </li>
+                                <li class="list-group-item me-5">
+                                    <i class="fa fa-retweet text-muted">  3.5K</i>
+                                </li>
+                                <li class="list-group-item me-5">
+                                    <i class="fa fa-heart text-muted">  35K</i>
+                                </li>
+                                 <li class="list-group-item me-5">
+                                    <i class="fa fa-upload text-muted me-5"></i>
+                                </li>
+                                
+                            </ul>
+                        </div>
+                 
+                    </div>
+                </div>
+            </li>
+             <li class="list-group-item">
+                <div class="row">
+                    <div class="col-1 p-0 ms-3">
+                        <img class="rounded-circle"
+                        width="50px"
+                        src="../../images/profile-image.jpg"/>
+                    </div>
+                    <div class="col-10 ms-2 pe-0">
+                        <span class="me-xl-2 fw-bold">Elon Musk</span><i class="fa fa-check-circle"></i>
+                        <span class="text-muted">@elonmusk.23h</span>
+                        </br>
+                        <span>Amazing how about @inspiration4x mission!</span>
+                        <div class="mt-2">
+                            <img width="100%"
+                            src="../../images/inspiration4x.jpg">
+                            <div class="ms-2">
+                                <p class="mb-2">Watch Countdown: Inspiration4 Mission to Space | Netflix Official Site</p>
+                                <span class="text-dark">From training to launch to landing, this all-access docuseries rides along with
+                                 the Inspiration4 crew on the first all-civilian orbital space mission.</span></br>
+                                 <i class="fa fa-link text-muted"> netflix.com</i>
+                            </div>
+                        </div>
+                        
+                        <div>
+                            <ul class="list-group list-group-horizontal">
+                                <li class="list-group-item me-5">
+                                    <i class="fa fa-comment text-muted">  4.2K</i>
+                                </li>
+                                <li class="list-group-item me-5">
+                                    <i class="fa fa-retweet text-muted">  3.5K</i>
+                                </li>
+                                <li class="list-group-item me-5">
+                                    <i class="fa fa-heart text-muted">  35K</i>
+                                </li>
+                                 <li class="list-group-item me-5">
+                                    <i class="fa fa-upload text-muted me-5"></i>
+                                </li>
+                                
+                            </ul>
                         </div>
                  
                     </div>

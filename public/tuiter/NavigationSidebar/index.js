1f75c9d79cb7139420ccd2dcaf6786efc71297ab
yangyulu <yangyulu1114@163.com>
2023-02-19 15:41:26 -0800

    delete unsued style

--- public/tuiter/NavigationSidebar/index.js
+++ public/tuiter/NavigationSidebar/index.js
@@ -18,35 +18,35 @@ const NavigationSidebar = (highlight) => {
            <span class="align-middle d-none ms-2 d-none d-xl-inline-block">Explore</span>
      </a>
      
-      <a class="list-group-item" href="/">
+      <a class="list-group-item ${highlight === 'Notifications' ? 'active' : ''}" href="/">
          <div class="input-group">
             <i class="fa fa-bell align-middle mt-1"> </i>
             <span class="align-middle ms-2 d-none d-xl-inline-block">Notifications</span>
          </div>
      </a>
      
-      <a class="list-group-item" href="/">
-          <i class="fa fa-envelope align-middle mt-1 ${highlight === 'Messages' ? 'active' : ''}"> </i>
+      <a class="list-group-item ${highlight === 'Messages' ? 'active' : ''}" href="/">
+          <i class="fa fa-envelope align-middle mt-1"> </i>
           <span class="align-middle ms-2 d-none d-xl-inline-block">Messages</span>
      </a>
      
-      <a class="list-group-item" href="/">
-         <i class="fa fa-bookmark align-middle mt-0 ${highlight === 'Bookmarks' ? 'active' : ''}"> </i>
+      <a class="list-group-item ${highlight === 'Bookmarks' ? 'active' : ''}" href="/">
+         <i class="fa fa-bookmark align-middle mt-0"> </i>
          <span class="align-middle ms-2 d-none d-xl-inline-block">Bookmarks</span>
      </a>
      
-      <a class="list-group-item" href="/">
-        <i class="fa fa-list align-middle mt-0 ${highlight === 'Lists' ? 'active' : ''}"> </i>
+      <a class="list-group-item ${highlight === 'Lists' ? 'active' : ''}" href="/">
+        <i class="fa fa-list align-middle mt-0"> </i>
         <span class="align-middle ms-2 d-none d-xl-inline-block">Lists</span>
      </a> 
      
-     <a class="list-group-item" href="/">
-          <i class="fa fa-user align-middle mt-0 ${highlight === 'Profile' ? 'active' : ''}"> </i>
+     <a class="list-group-item ${highlight === 'Profile' ? 'active' : ''}" href="/">
+          <i class="fa fa-user align-middle mt-0"> </i>
           <span class="align-middle ms-2 d-none d-xl-inline-block">Profile</span>
      </a>
      
-      <a class="list-group-item" href="/">
-          <i class="fa fa-ellipsis-h align-middle mt-0 ${highlight === 'More' ? 'active' : ''}"> </i>
+      <a class="list-group-item ${highlight === 'More' ? 'active' : ''}" href="/">
+          <i class="fa fa-ellipsis-h align-middle mt-0"> </i>
           <span class="align-middle ms-2 d-none d-xl-inline-block">More</span>
      </a>
      

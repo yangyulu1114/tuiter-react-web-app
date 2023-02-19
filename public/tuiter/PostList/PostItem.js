eea080579bb4201dd36a1e3ddeeaebdcdf1548ff
yangyulu <yangyulu1114@163.com>
2023-02-19 15:14:46 -0800

    postItem

--- public/tuiter/PostList/PostItem.js
+++ public/tuiter/PostList/PostItem.js
@@ -1,9 +1,11 @@
 /* eslint-env jquery */
 
 const PostItem = (post) => {
+    let emptyTitleAfter = (post.titleAfter == null)
     let emptySubtitle = (post.subtitle == null);
     let emptyContent = (post.content == null);
     let emptySource = (post.source == null);
+    let emptyDiv = (emptySubtitle && emptyContent && emptySource)
     return(`  
            <li class="list-group-item bg-black">
                 <div class="row">
@@ -16,16 +18,16 @@ const PostItem = (post) => {
                         <span class="me-xl-2 fw-bold">${post.userName}</span><i class="fa fa-check-circle"></i>
                         <span class="text-muted">@${post.handle}.${post.time}</span>
                         </br>
-                        <span>${post.titleBefore}</span><span class="text-primary"> ${post.titleHighlight}</span><span>${post.titleAfter}</span>
+                        <span>${post.titleBefore}</span><span class="text-primary"> ${post.titleHighlight}</span>${emptyTitleAfter ? '' : `<span>${post.titleAfter}</span>`}
                         <div class="mt-2 border border-1 border-light rounded-corners-inline">
-                            <img class="mt-1 border border-top-0 border-start-0 border-end-0 border-light" width="100%"
+                            <img class="border border-top-0 border-start-0 border-end-0 border-light rounded-corners-inline" width="100%"
                             src="${post.image}"
                             >
-                            <div class="ms-3 mb-2">
+                            <div class="ms-3">
                                ${emptySubtitle ? '' : `<p className="mb-2">${post.subtitle}</p>`}  
                                ${emptyContent ? '' : `<span class="text-dark">${post.content}</span></br>`}
-                               ${emptySource ? '' : `<i class="fa fa-link text-muted"> ${post.source}</i>`}  
                             </div>
+                               ${emptySource ? '' : `<i class="fa fa-link text-muted mb-2 ms-3"> ${post.source}</i>`}  
                         </div>
                         
                         <div>

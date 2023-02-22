1aba83701b65916a3f159d165276ff9a546873b2
yangyulu <yangyulu1114@163.com>
2023-02-22 15:48:31 -0800

    Implementing navigation in Single Page Applications

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 import Labs from "./labs";
 import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
+import {BrowserRouter} from "react-router-dom";
+import {Routes, Route} from "react-router";
 
 function App() {
   return (
-      <div className="container">
-        {/*<h1>Hello World!</h1>*/}
-          <HelloWorld/>
-          <Labs/>
-          <Tuiter/>
-      </div>
+      <BrowserRouter>
+          <div className="container">
+            {/*<h1>Hello World!</h1>*/}
+              <Routes>
+                  <Route index
+                         element={<Labs/>}/>
+                  <Route path="/hello"
+                         element={<HelloWorld/>}/>
+                  <Route path="/tuiter"
+                         element={<Tuiter/>}/>
+              </Routes>
+
+          </div>
+      </BrowserRouter>
   );
 }
 

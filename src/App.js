5fc7d223682af825a60666d2b3ea04c647ca4c3a
yangyulu <yangyulu1114@163.com>
2023-02-21 22:57:47 -0800

    Breaking out Hello World into a separate component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Labs from "./labs";
-
+import HelloWorld from "./labs/a6/hello-world";
 
 function App() {
   return (
       <div className="container">
-        <h1>Hello World!</h1>
-        <Labs/>
+        {/*<h1>Hello World!</h1>*/}
+          <HelloWorld/>
+          <Labs/>
       </div>
   );
 }

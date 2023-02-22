93fa623d9a406081ea1e5963635d32fec69fc36e
yangyulu <yangyulu1114@163.com>
2023-02-21 22:53:37 -0800

    Breaking out assignments into separate components

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,7 +1,10 @@
+import Assignment6 from "./a6";
+
 function Labs() {
     return(
         <div>
-            <h1>Assignment 6</h1>
+            {/*<h1>Assignment 6</h1>*/}
+            <Assignment6/>
         </div>
     );
 }

7d222fe259dbfb22a4709a72ceef4602f8627fd7
yangyulu <yangyulu1114@163.com>
2023-02-23 11:13:27 -0800

    Working with HTML classes

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,12 +1,12 @@
 import {Link}
     from "react-router-dom";
+import Classes from "./classes";
+
 function Assignment6() {
     return (
         <div>
-            <Link to="/">Lab</Link> |
-            <Link to="/hello">Hello</Link> |
-            <Link to="/tuiter">Tuiter</Link>
             <h1>Assignment 6</h1>
+            <Classes/>
         </div>
     );
 }

fb26bcbfb2cbe520ccd8962cf105754315ca5ae1
yangyulu <yangyulu1114@163.com>
2023-02-22 16:36:41 -0800

    Navigating with links in SPAs

--- src/labs/a6/hello-world.js
+++ src/labs/a6/hello-world.js
@@ -1,6 +1,13 @@
+import {Link}
+    from "react-router-dom";
 function HelloWorld() {
     return(
-        <h1>Hello World!</h1>
+        <div>
+            <Link to="/">Lab</Link> |
+            <Link to="/hello">Hello</Link> |
+            <Link to="/tuiter">Tuiter</Link>
+            <h1>Hello World!</h1>
+        </div>
     );
 };
 export default HelloWorld;

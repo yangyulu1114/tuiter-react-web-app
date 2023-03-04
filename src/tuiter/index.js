569d4204de1e66a1e3f126fca24cd9fff7a1d9f2
yangyulu <yangyulu1114@163.com>
2023-03-03 17:48:14 -0800

    Re-implement WhoToFollowListItem

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -3,6 +3,9 @@ import {Link}
 import Nav from "../nav";
 import NavigationSidebar
     from "./navigation-sidebar";
+import WhoToFollowListItem
+    from "./who-to-follow-list/who-to-follow-list-item";
+
 function Tuiter() {
     return(
         <div>
@@ -11,6 +14,18 @@ function Tuiter() {
             {/*<Link to="/tuiter">Tuiter</Link>*/}
             <Nav/>
             <NavigationSidebar active="home"/>
+            <WhoToFollowListItem who={{
+                userName: 'NASA', handle: 'NASA',
+                avatarIcon: 'NASA.JPG',
+            }}/>
+            <WhoToFollowListItem who={{
+                userName: 'Tesla', handle: 'tesla',
+                avatarIcon: 'TESLA.JPG',
+            }}/>
+            <WhoToFollowListItem who={{
+                userName: 'SpaceX', handle: 'SpaceX',
+                avatarIcon: 'spaceX_logo.jpg',
+            }}/>
             <h1>Tuiter</h1>
         </div>
     );

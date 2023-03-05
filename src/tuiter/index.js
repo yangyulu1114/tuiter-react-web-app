ee7e53960b50c3c48e408a54c63871754844b6e2
yangyulu <yangyulu1114@163.com>
2023-03-05 00:06:16 -0800

    change link

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -9,17 +9,29 @@ import WhoToFollowList from "./who-to-follow-list";
 import PostSummaryList from "./post-summary-list";
 import ExploreComponent from "./explore";
 import HomeComponent from "./home";
+import {BrowserRouter} from "react-router-dom";
+import {Route, Routes} from "react-router";
+import Labs from "../labs";
 
 function Tuiter() {
     return(
         <div className="row mt-2">
             <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                <NavigationSidebar active="explore"/>
+                {/*<NavigationSidebar active="explore"/>*/}
+                <Routes>
+                    <Route index element={<NavigationSidebar active="explore"/>}/>
+                    <Route path="/home"    element={<NavigationSidebar active="home"/>}/>
+                    <Route path="/explore" element={<NavigationSidebar active="explore"/>}/>
+                </Routes>
             </div>
             <div className="col-10 col-md-10 col-lg-7 col-xl-6"
                  style={{"position": "relative"}}>
                 {/*<ExploreComponent/>*/}
-                <HomeComponent/>
+                <Routes>
+                    <Route index element={<ExploreComponent/>}/>
+                    <Route path="/home"    element={<HomeComponent/>}/>
+                    <Route path="/explore" element={<ExploreComponent/>}/>
+                </Routes>
             </div>
             <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
                 <WhoToFollowList/>

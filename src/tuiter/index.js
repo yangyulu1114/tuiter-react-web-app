9a07e67165d6824ab15b8ecfd3ceab11181599ac
yangyulu <yangyulu1114@163.com>
2023-03-05 09:49:36 -0800

    change layout

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -15,11 +15,11 @@ import Labs from "../labs";
 
 function Tuiter() {
     return(
-        <div className="row mt-2">
+        <div className="row mt-2 mb-2">
             <div className="col-2 col-md-2 col-lg-1 col-xl-2">
                 <NavigationSidebar active="explore"/>
             </div>
-            <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+            <div className="col-10 col-lg-7 col-xl-6"
                  style={{"position": "relative"}}>
                 {/*<ExploreComponent/>*/}
                 <Routes>
@@ -28,7 +28,7 @@ function Tuiter() {
                     <Route path="/explore" element={<ExploreComponent/>}/>
                 </Routes>
             </div>
-            <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+            <div className="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
                 <WhoToFollowList/>
             </div>
         </div>

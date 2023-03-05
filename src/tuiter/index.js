d9a5fd69df48529eae3c8505d235425472af9e89
yangyulu <yangyulu1114@163.com>
2023-03-04 19:37:20 -0800

    add sidebar icon

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -12,14 +12,14 @@ import ExploreComponent from "./explore";
 function Tuiter() {
     return(
         <div className="row mt-2">
-            <div className="col-3 col-md-3 col-lg-2 col-xl-2">
+            <div className="col-2 col-md-2 col-lg-1 col-xl-2">
                 <NavigationSidebar active="explore"/>
             </div>
-            <div className="col-9 col-md-9 col-lg-7 col-xl-6"
+            <div className="col-10 col-md-10 col-lg-7 col-xl-6"
                  style={{"position": "relative"}}>
                 <ExploreComponent/>
             </div>
-            <div className="d-sm-none d-md-none d-lg-block col-lg-3 col-xl-4">
+            <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
                 <WhoToFollowList/>
             </div>
         </div>
--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -7,38 +7,48 @@ const NavigationSidebar = (
 ) => {
     return (
         <div className="list-group">
-            <a className="list-group-item">Tuiter</a>
+            <a className="list-group-item">
+                <i className="bi bi-twitter"></i>
+            </a>
             <a className={`list-group-item
-                    ${active === 'home'?'active':''}`}>
-                Home
+                ${active === 'home'?'active':''}`}>
+                <i className="bi bi-house-fill align-middle mt-1"></i>
+                <span className="align-middle ms-2 d-none d-xl-inline-block">Home</span>
             </a>
             <a className={`list-group-item
                     ${active === 'explore'?'active':''}`}>
-                Explore
+                <i className="bi bi-hash align-middle"></i>
+                <span className="align-middle d-none ms-2 d-none d-xl-inline-block">Explore</span>
             </a>
             <a className={`list-group-item
                     ${active === 'notifications'?'active':''}`}>
-                Notifications
+                <i className="bi bi-bell-fill align-middle mt-1"> </i>
+                <span className="align-middle ms-2 d-none d-xl-inline-block">Notifications</span>
             </a>
             <a className={`list-group-item
                     ${active === 'messages'?'active':''}`}>
-                Messages
+                <i className="bi bi-envelope-fill align-middle mt-1"> </i>
+                <span className="align-middle ms-2 d-none d-xl-inline-block">Messages</span>
             </a>
             <a className={`list-group-item
                     ${active === 'bookmarks'?'active':''}`}>
-                Bookmarks
+                <i className="bi bi-bookmark-fill align-middle mt-0"> </i>
+                <span className="align-middle ms-2 d-none d-xl-inline-block">Bookmarks</span>
             </a>
             <a className={`list-group-item
                     ${active === 'lists'?'active':''}`}>
-                Lists
+                <i className="bi bi-list-ul align-middle mt-0"> </i>
+                <span className="align-middle ms-2 d-none d-xl-inline-block">Lists</span>
             </a>
             <a className={`list-group-item
                     ${active === 'profile'?'active':''}`}>
-                Profile
+                <i className="bi bi-person-fill align-middle mt-0"> </i>
+                <span className="align-middle ms-2 d-none d-xl-inline-block">Profile</span>
             </a>
             <a className={`list-group-item
                     ${active === 'more'?'active':''}`}>
-                More
+                <i className="bi bi-three-dots align-middle mt-0"> </i>
+                <span className="align-middle ms-2 d-none d-xl-inline-block">More</span>
             </a>
         </div>
     );

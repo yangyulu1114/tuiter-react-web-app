889e99d23ca38c61876508db3bd9cbd3d87f4dec
yangyulu <yangyulu1114@163.com>
2023-03-04 21:35:31 -0800

    change style

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -7,16 +7,16 @@ const ExploreComponent = () => {
             <div className="row">
                 <div className="col-11 position-relative">
                     <input placeholder="Search Tuiter"
-                           className="form-control rounded-pill ps-5"/>
+                           className="form-control rounded-pill ps-5 pb-2"/>
                     <i className="bi bi-search position-absolute
-                       wd-nudge-up"></i>
+                       wd-tab-nudge-bottom"></i>
                 </div>
                 <div className="col-1">
                     <i className="wd-bottom-4 text-primary float-end bi
                        bi-gear-fill fs-2 position-relative"></i>
                 </div>
             </div>
-            <ul className="nav nav-pills mb-2">
+            <ul className="nav nav-pills mb-2 mt-1">
                 <li className="nav-item">
                     <a className="nav-link active">For You</a>
                 </li>

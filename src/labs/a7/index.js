73b9bfb6bf0e8cae0c3b367327f85072d3d9527f
yangyulu <yangyulu1114@163.com>
2023-03-05 22:34:51 -0800

    Create a Redux Examples component

--- src/labs/a7/index.js
+++ src/labs/a7/index.js
@@ -1,8 +1,10 @@
 import React from "react";
+import ReduxExamples from "./redux-examples";
 const Assignment7 = () => {
     return(
         <>
             <h1>Assignment 7</h1>
+            <ReduxExamples/>
         </>
     );
 };

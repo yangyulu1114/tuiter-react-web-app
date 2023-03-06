789ce16752b175f089936c8b22ceeb4fd7f85078
yangyulu <yangyulu1114@163.com>
2023-03-06 09:29:25 -0800

    Create a Hello World Redux component

--- src/labs/a7/redux-examples/index.js
+++ src/labs/a7/redux-examples/index.js
@@ -1,10 +1,18 @@
 import React from "react";
-
+import HelloReduxExampleComponent
+    from "./hello-redux-example-component";
+import hello from "./reducers/hello";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+const store = createStore(hello);
 const ReduxExamples = () => {
     return(
-        <div>
-            <h2>Redux Examples</h2>
-        </div>
+        <Provider store={store}>
+            <div>
+                <h2>Redux Examples</h2>
+                <HelloReduxExampleComponent/>
+            </div>
+        </Provider>
     );
 };
 

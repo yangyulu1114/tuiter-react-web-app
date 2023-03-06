025b110ece9b87073f2e1863cfa8c2d140a54577
yangyulu <yangyulu1114@163.com>
2023-03-06 09:57:20 -0800

    Handling application level events

--- src/labs/a7/redux-examples/todos-component.js
+++ src/labs/a7/redux-examples/todos-component.js
@@ -1,9 +1,15 @@
 import React, {useState} from "react";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {addTodo} from "./reducers/todos-reducer";
 const Todos = () => {
     const todos
         = useSelector(state => state.todos);
     const [todo, setTodo] = useState({do: ''});
+    const dispatch = useDispatch();
+    const createTodoClickHandler = () => {
+        dispatch(addTodo(todo))
+    }
+
     const todoChangeHandler = (event) => {
         const doValue = event.target.value;
         const newTodo = {
@@ -17,10 +23,14 @@ const Todos = () => {
             <h3>Todos</h3>
             <ul className="list-group">
                 <li className="list-group-item">
+                    <button onClick={createTodoClickHandler}
+                            className="btn btn-primary w-25
+                          float-end">
+                        Create</button>
                     <input
                         onChange={todoChangeHandler}
                         value={todo.do}
-                        className="form-control"/>
+                        className="form-control w-75"/>
                 </li>
                 {
                     todos.map(todo =>

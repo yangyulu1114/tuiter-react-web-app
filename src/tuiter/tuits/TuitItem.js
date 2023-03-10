1fbc0809f6a277b98bf138ebf52821eeda3c61f5
yangyulu <yangyulu1114@163.com>
2023-03-10 14:26:01 -0800

    Deleting Tuits

--- src/tuiter/tuits/TuitItem.js
+++ src/tuiter/tuits/TuitItem.js
@@ -1,7 +1,14 @@
 import React from "react";
 import TuitStats from "./TuitStats";
+import {useDispatch} from "react-redux";
+import {deleteTuit} from "./tuits-reducer";
 
 const TuitItem = ({tuit}) => {
+    const dispatch = useDispatch();
+    const deleteTuitHandler = (id) => {
+        dispatch(deleteTuit(id));
+    }
+
     return (
         <li className="list-group-item">
             <div className="row ms-0 me-0">
@@ -11,6 +18,8 @@ const TuitItem = ({tuit}) => {
                          src={`/images/${tuit.image}`}/>
                 </div>
                 <div className="col-10 ms-2 pe-0">
+                    <i className="bi bi-x-lg float-end"
+                       onClick={() => deleteTuitHandler(tuit._id)}></i>
                     <span className="me-xl-2 fw-bold">{tuit.userName}</span><i className="bi bi-patch-check-fill wd-color-blue"></i>
                     <span className="text-muted"> {tuit.handle}.{tuit.time}</span>
                     <br/>

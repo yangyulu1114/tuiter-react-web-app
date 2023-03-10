19046a5f2c958dc5c5e98856f1deae0caf68460e
yangyulu <yangyulu1114@163.com>
2023-03-10 14:20:42 -0800

    Creating Tuits

--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -1,9 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
 import tuits from './tuits.json';
 
+const currentUser = {
+    "userName": "NASA",
+    "handle": "@nasa",
+    "image": "nasa-logo.png",
+};
+
+const templateTuit = {
+    ...currentUser,
+    "topic": "Space",
+    "time": "2h",
+    "liked": false,
+    "replies": 0,
+    "retuits": 0,
+    "likes": 0,
+}
+
+
+
 const tuitsSlice = createSlice({
     name: 'tuits',
-    initialState: tuits
+    initialState: tuits,
+    reducers: {
+        createTuit(state, action) {
+            state.unshift({
+                ...action.payload,
+                ...templateTuit,
+                _id: (new Date()).getTime(),
+            })
+        }
+    }
 });
-
+export const {createTuit} = tuitsSlice.actions;
 export default tuitsSlice.reducer;

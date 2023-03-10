3ed118111d93da09187e6e257916e20a37a37971
yangyulu <yangyulu1114@163.com>
2023-03-10 09:56:05 -0800

    implement home using redux

--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import PostSummaryItem
     from "./post-summary-item";
 import {useSelector} from "react-redux";
-import whoArray from "../data/who.json";
-import WhoToFollowListItem from "../who-to-follow-list/who-to-follow-list-item";
 
 const PostSummaryList = () => {
     const postsArray = useSelector(state => state.tuits)
--- src/tuiter/tuits/TuitsList.js
+++ src/tuiter/tuits/TuitsList.js
@@ -1,7 +1,8 @@
-import tuitsArray from "./tuits.json";
 import React from "react";
 import TuitItem from "./TuitItem";
+import {useSelector} from "react-redux";
 const TuitsList = () => {
+    const tuitsArray = useSelector(state => state.tuits)
     return (
         <div>
             <h3>Home</h3>

a78b8b2fd13025da2dd69a7e00e6096bd506859a
yangyulu <yangyulu1114@163.com>
2023-03-10 10:55:02 -0800

    Creating a form

--- src/tuiter/tuits/TuitsList.js
+++ src/tuiter/tuits/TuitsList.js
@@ -1,12 +1,14 @@
 import React from "react";
 import TuitItem from "./TuitItem";
 import {useSelector} from "react-redux";
+import WhatsHappening from "../home/whats-happening";
 const TuitsList = () => {
     const tuitsArray = useSelector(state => state.tuits)
     return (
         <div>
             <h3>Home</h3>
-        <ul className="list-group">
+            <WhatsHappening/>
+            <ul className="list-group">
             {
                 tuitsArray.map(tuit =>
                     <TuitItem

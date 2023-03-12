c3d4e6989a9b9ae05a8c4da64a142a8c30f586ba
yangyulu <yangyulu1114@163.com>
2023-03-12 11:54:36 -0700

    edit profil

--- src/tuiter/edit-profile/index.js
+++ src/tuiter/edit-profile/index.js
@@ -1,9 +1,41 @@
 import React from "react";
 import {useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import "../profile/index.css"
 
 const EditProfileComponent = () => {
-    return(
-      <h3> EditProfile</h3>
+    const profile = useSelector((state => state.profile));
+
+    let navigate = useNavigate();
+    const routeChange = () =>{
+        let path = `../profile`;
+        navigate(path);
+    }
+    return (
+        <div className="border border-thin">
+            <div>
+                <div className="mt-1 mb-2 row">
+                    <div className="col-1 ms-3">
+                        <i className="bi bi-x-lg"></i>
+                    </div>
+                    <div className="col-8 ms-2">
+                        <span>Edit Profile</span>
+                    </div>
+                    <div className="col-2">
+                        <button className="rounded-pill btn border float-end wd-font-13 btn-dark ps-3 pe-3 pt-1 pb-1" onClick={routeChange}>
+                              Save
+                        </button>
+                    </div>
+
+                </div>
+                <img width="100%" height="50%" src={`/images/${profile.bannerPicture}`}/>
+                <img className="rounded-circle wd-relative" width={100} src={`/images/${profile.profilePicture}`}/>
+                <div className="ms-3 wd-relative-top">
+                    <input value={`${profile.firstName} ${profile.lastName}`}/>
+                </div>
+
+            </div>
+        </div>
     );
 };
 

b5048ddfcd724df5e1f63af9eed63242eac13e6a
yangyulu <yangyulu1114@163.com>
2023-03-26 22:18:42 -0700

    Integrating React applications with RESTful Web service APIs

---
+++ src/services/tuits-service.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+const TUITS_API = 'http://localhost:4000/api/tuits';
+
+export const createTuit = async (tuit) => {}
+export const findTuits  = async ()     => {}
+export const deleteTuit = async (tuit) => {}
+export const updateTuit = async (tuit) => {}

8ef60c8a26580b4a390c6349838bf415025d28ca
yangyulu <yangyulu1114@163.com>
2023-04-02 14:47:38 -0700

    dummy push

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-// const TUITS_API = 'https://tuiter-node-server-app-c30d.onrender.com/api/tuits';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`
 export const createTuit = async (tuit) => {

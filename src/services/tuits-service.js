abe4e8601a57b6df943089390c192bdc52bbd31d
yangyulu <yangyulu1114@163.com>
2023-04-02 14:38:16 -0700

    API_BASE

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
-const TUITS_API = 'http://localhost:4000/api/tuits';
-
+// const TUITS_API = 'https://tuiter-node-server-app-c30d.onrender.com/api/tuits';
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
     return response.data;
 }
 export const findTuits  = async () => {
+    console.log(TUITS_API);
     const response = await axios.get(TUITS_API);
     const tuits = response.data;
     return tuits;

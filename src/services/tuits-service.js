61c941da04626c9aa11442ff0db75facbf6ee0c0
yangyulu <yangyulu1114@163.com>
2023-04-05 10:41:35 -0700

    a9 first commit

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`
 export const createTuit = async (tuit) => {
-    const response = await axios.post(TUITS_API, tuit)
+    const response = await axios.post(TUITS_API, tuit);
     return response.data;
 }
 export const findTuits  = async () => {
@@ -12,9 +12,10 @@ export const findTuits  = async () => {
     return tuits;
 }
 export const deleteTuit = async (tid) => {
+    console.log(tid);
     const response = await axios
-        .delete(`${TUITS_API}/${tid}`)
-    return response.data
+        .delete(`${TUITS_API}/${tid}`);
+    return response.data;
 }
 export const updateTuit = async (tuit) => {
     const response = await axios
